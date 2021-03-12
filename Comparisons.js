08f72c5b960a1984f9270091052d99251844ed5a
Busehzrc <77810671+Busehzrc@users.noreply.github.com>
2021-03-12 11:25:40 +0000

    Learned Comparisons

---
+++ Comparisons.js
@@ -0,0 +1,31 @@
+
+
+let price="100"
+let username="Buse"
+// == Esitse
+
+console.log(price==1)
+console.log(price==100)
+
+// Hem deger hem turu esitse
+console.log(price===1)
+console.log(price===100)
+
+//Esit degilse : Eger misafir degilse
+console.log(username!= "guest")
+
+//Kucuktur Buyuktur Esittir
+console.log(price>100)
+
+console.log(price>=100)
+
+// && ve 
+
+price=0
+console.log(price > 0 && username!= "guest")
+
+// || or birden fazla oldugunda herhangi biri gecerli
+
+console.log(price > 0 ||  username!= "guest")
+
+
