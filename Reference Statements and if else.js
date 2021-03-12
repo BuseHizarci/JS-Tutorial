f495b861e351649dcd75ef16a7ac7ec37f278447
Busehzrc <77810671+Busehzrc@users.noreply.github.com>
2021-03-12 14:12:24 +0000

    if else - else if statements

--- Reference Statements and if else.js
+++ Reference Statements and if else.js
@@ -15,16 +15,59 @@
 // }
 
 // Coklu Kosullarla Calismak 
-let username=prompt("Kullanici adi giriniz")
-let age=prompt("Yasiniz nedir")
-let info=document.querySelector("#info") // Htmldeki infoyu alip onun yerine cikan sonuclari yazdirdim
+// let username=prompt("Kullanici adi giriniz")
+// let age=prompt("Yasiniz nedir")
+// let info=document.querySelector("#info") // Htmldeki infoyu alip onun yerine cikan sonuclari yazdirdim
 
-if (username && age >= 18){
-    info.innerHTML="Ehliyet alabilir"
+// if (username && age >= 18){
+//     info.innerHTML="Ehliyet alabilir"
+// }
+// else if (!username) { // username yok
+//     info.innerHTML="Kullanici adi yoktur"
+// }
+// else if(!(age >= 18)){ // yas yok veya 18 degil
+//     info.innerHTML="Yas bilginiz yok veya 18 yasindan kucuksunuz"
+// }
+//let username=prompt("Kullanici bilgilerinizi giriniz")
+//let info= document.querySelector("#info")
+
+//ternary kullanimi 
+//kosul ? dogruysa " yanlissa "
+//info.innerHTML= `${username.length > 0 ? username:"Kullanici bilgisi yok"}`
+
+
+let examGrade=prompt("Puan Gir")
+let textInfo;
+if (examGrade >= 0  && examGrade<= 100){
+   
+     info.classList.add('text-primary')
+
+    if (examGrade >= 90) {
+        textInfo="AA"
+    
+    } 
+    else if(examGrade >= 85){
+        textInfo="BA"
+    }
+    
+    else if(examGrade >= 60){
+        textInfo="CC"
+    }
+    else if(examGrade >= 59){
+        textInfo="DD"
+    }
+    else if(examGrade <= 20){
+
+        textInfo="FF"
+    }
+    else{
+        textInfo = "Bilgiler Yanlis"
+        info.classList.remove('text-primary')
+        info.classList.add('text-danger')
+    }
+
 }
-else if (!username) { // username yok
-    info.innerHTML="Kullanici adi yoktur"
-}
-else if(!(age >= 18)){ // yas yok veya 18 degil
-    info.innerHTML="Yas bilginiz yok veya 18 yasindan kucuksunuz"
-}
+let info= document.querySelector("#info")
+info.innerHTML= `${textInfo} -> ${examGrade}` 
+
+
