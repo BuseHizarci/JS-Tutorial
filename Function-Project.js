063f88389d502198a455b2dbaddcb3f3159ef0e1
Busehzrc <77810671+Busehzrc@users.noreply.github.com>
2021-03-14 21:30:38 +0000

    String to number

--- Function-Project.js
+++ Function-Project.js
@@ -1,5 +1,7 @@
 // FOnce tanimlamalar 
-let counter=0
+//Number a cevirdim yanyana degil toplayarak yazsin
+//locall storagedan aldigim bilgi varsa number olarak gorulsun artsin
+let counter=localStorage.setItem('counter') ? Number(localStorage.getItem('counter'))
 let counterDOM=document.querySelector('#counter')
 let increaseDOM=document.querySelector('#increase')
 let decreaseDOM=document.querySelector('#decrease')
@@ -12,7 +14,7 @@ increaseDOM.addEventListener("click",clickEvent)  // click oldugunda clickevent 
 decreaseDOM.addEventListener("click",clickEvent) 
 
     function clickEvent(){
-        console.log(this.id)
         this.id=="increase" ? counterDOM.innerHTML = counter +=1 :  counterDOM.innerHTML = counter -=1
-        
+        localStorage.setItem('counter',counter) //
+        counterDOM.innerHTML=counter
     }  //this id icerisine ulastim eger inc ise +1 dec ise -1
